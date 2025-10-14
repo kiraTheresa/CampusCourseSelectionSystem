67b8d35fce9d3169153d79306a9b4ca8d0748800
kiraTheresa <2312190107@pop.zjgsu.edu.cn>
2025-10-14 20:20:04 +0800

    1.1. 项目初始化

---
+++ src/main/java/com/zjgsu/ms/hxy/CampusCourseSelectionSystem/CampusCourseSelectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zjgsu.ms.hxy.CampusCourseSelectionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCourseSelectionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCourseSelectionSystemApplication.class, args);
+	}
+
+}
